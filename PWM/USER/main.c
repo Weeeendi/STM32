f430979fa351329c7df18cd40666f6792df8a888
wendi <wendi1078771091@gmail.com>
2019-04-08 16:57:21 +0800

    Update PWM

--- PWM/USER/main.c
+++ PWM/USER/main.c
@@ -12,13 +12,12 @@
  作者：正点原子 @ALIENTEK
 ************************************************/
 
-#include "digital.h"
-#include "Systick.h"
-
-u8 digital_table[]={0xc0,0xf9,0xA4,0xB0,0x99,0x92,0x82,0xf8,0x80,0x90,0x88,0x83,0xC6,0xA1,0x86,0x8E};//0-F 共阳
 
 int main(void)
- {	    GPIO_InitTypeDef GPIO_InitStructure;
+ {	    static u16 dutyCycle=0 ;
+   
+   
+        GPIO_InitTypeDef GPIO_InitStructure;
         TIM_TimeBaseInitTypeDef TIM_TImeBaseStructure;
         TIM_OCInitTypeDef  TIM_OCInitStructure;
         //使能GPIOA，TIM2
@@ -37,22 +36,20 @@ int main(void)
         
 
         //配置TIM2的时钟输出频率，以及其它相关参数初始化
-        TIM_TImeBaseStructure.TIM_Prescaler=360-1;//设置PWM的频率
+        TIM_TImeBaseStructure.TIM_Prescaler=0;//设置PWM的频率
         TIM_TImeBaseStructure.TIM_CounterMode=0;
-        TIM_TImeBaseStructure.TIM_Period=100;
+        TIM_TImeBaseStructure.TIM_Period=7200-1;
         TIM_TimeBaseInit(TIM2,&TIM_TImeBaseStructure);
 
 
         //设置PWM的输出方式
         TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
         TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-        TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
         TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-        TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
-        TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
-        TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;   
+       
+       
         //配置占空比
-        TIM_OCInitStructure.TIM_Pulse=20;
+        //TIM_OCInitStructure.TIM_Pulse=20;
 
         TIM_OC2Init(TIM2,&TIM_OCInitStructure);
         TIM_ForcedOC1Config(TIM2,TIM_ForcedAction_Active);
@@ -62,7 +59,21 @@ int main(void)
         
         while(1)
         {
-                ;
+              while (dutyCycle < 9999)
+            {
+            dutyCycle ++;
+          //	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, dutyCycle);//修改占空比后刷新寄存器设置
+              TIM2->CCR2 = dutyCycle;
+		  
+            }
+    
+              while (dutyCycle)
+              {
+                dutyCycle --;
+            //  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, dutyCycle);
+                TIM2->CCR2 = dutyCycle;
+              }
+          
         }
  }
 
