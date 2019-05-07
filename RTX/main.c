9aa147213a03e11dc36c51a9bf9ef435c6462d34
wendi <wendi1078771091@gmail.com>
2019-05-07 10:14:09 +0800

    RTX

--- RTX/main.c
+++ RTX/main.c
@@ -6,19 +6,30 @@
 #include <cmsis_os.h>
 #include "led.h"
 
+/*----------------------------------------------------------------------------
+  Simple delay routine
+ *---------------------------------------------------------------------------*/
+void delay (unsigned int count)
+{
+unsigned int index;
 
+	for(index =0;index<count;index++)
+	{
+		;
+	}
+}
 
 /*----------------------------------------------------------------------------
   Flash LED 1
  *---------------------------------------------------------------------------*/
 void Thread_LED1(void const* argument)
 {
-  while(1)
+  for(;;)
   {
     LED_On(0);
-    osDelay(200);
+    delay(1500);
     LED_Off(0);
-    osDelay(200);
+    delay(1500);
   }
 }
 
@@ -27,18 +38,18 @@ void Thread_LED1(void const* argument)
  *---------------------------------------------------------------------------*/
 void Thread_LED2(void const* argument)
 {
-  while(1)
+  for(;;)
   {
     LED_On(1);
-    osDelay(500);
+    delay(1500);
     LED_Off(1);
-    osDelay(200);
+    delay(1500);
   }
 }
 
 osThreadId mai_ID,Led_ID1,Led_ID2;
-osThreadDef(Thread_LED1,osPriorityNormal,1,0);
-osThreadDef(Thread_LED2,osPriorityNormal,1,0);
+//osThreadDef(Thread_LED1,osPriorityAboveNormal,1,0);
+osThreadDef(Thread_LED2,osPriorityNormal,2,0);
 /*
  * main: initialize and start the system
  */
@@ -46,8 +57,8 @@ int main (void) {
   osKernelInitialize ();                    // initialize CMSIS-RTOS
   LED_Initialize();
   // initialize peripherals here
-  Led_ID2 = osThreadCreate(osThread(Thread_LED1),NULL);
-  Led_ID1 = osThreadCreate(osThread(Thread_LED2),NULL);
+ // Led_ID1 = osThreadCreate(osThread(Thread_LED1),NULL);
+  Led_ID2 = osThreadCreate(osThread(Thread_LED2),NULL);
   // create 'thread' functions that start executing,
   
   // example: tid_name = osThreadCreate (osThread(name), NULL);
