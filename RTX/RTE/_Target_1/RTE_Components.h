a40de70d53dbabd714f0c4f983ba0b8f363d9628
w117 <wendi1078771091@gmail.com>
2019-05-14 20:46:25 +0800

    5/14

--- RTX/RTE/_Target_1/RTE_Components.h
+++ RTX/RTE/_Target_1/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Configuration File
+ * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'test1' 
@@ -16,15 +16,10 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
-/*  ARM::CMSIS:RTOS:Keil RTX:4.81.1 */
 #define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
         #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
-/*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
 #define RTE_DEVICE_STDPERIPH_FRAMEWORK
-/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_GPIO
-/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_RCC
-
 
 #endif /* RTE_COMPONENTS_H */
